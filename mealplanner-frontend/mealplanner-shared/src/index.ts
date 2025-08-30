acee9b7626ff4690dd34ce08c7a5bb8c669dc7c2
Elda Moudy <elda@moudy.net>
2025-08-30 10:27:12 -0400

    moving mock data to mealplanning-shared.

--- mealplanner-frontend/mealplanner-shared/src/index.ts
+++ mealplanner-frontend/mealplanner-shared/src/index.ts
@@ -6,6 +6,7 @@ export * from "./constants/index.js";
 export * from "./api-client/index.js";
 export * from "./utils/index.js";
 export * from "./hooks/index.js";
+export * from "./mocks/mock-data.js";
 
 // Version export
 export const VERSION = "1.0.0";
