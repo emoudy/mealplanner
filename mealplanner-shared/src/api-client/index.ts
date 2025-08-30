cf86f02f4851619d3b118417edf7ff61e5d46d0c
Elda Moudy <elda@moudy.net>
2025-08-30 10:27:24 -0400

    removing legacy folder

--- mealplanner-shared/src/api-client/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export API client components
-export * from "./client.js";
-export * from "./services.js";
-
-// Re-export for convenience
-export { createApiClient, ApiClient, ApiError } from "./client.js";
-export { FlavorBotApi } from "./services.js";
--- mealplanner-shared/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// Re-export all hooks
-export * from "./useApi.js";
-export * from "./useAuth.js";
-export * from "./useRecipes.js";
-
-// Additional hooks can be added here as they are created
-// export * from "./useChat.js";
-// export * from "./useUsage.js";
-// export * from "./useUser.js";
